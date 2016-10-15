8090ada083efb82af12bbb36c9255b82e87c3962
sivant <sivantc1990@gmail.com>
2016-10-15 19:41:10 +0300

    new proj

--- app/src/main/java/com/blingbling/sivant/blingbling/MainActivity.java
+++ app/src/main/java/com/blingbling/sivant/blingbling/MainActivity.java
@@ -1,13 +1,33 @@
 package com.blingbling.sivant.blingbling;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button_register = (Button) findViewById(R.id.button_register);
+        button_register.setOnClickListener(this);
     }
-}
+        public void onClick (View v) {
+            switch(v.getId()) {
+                case R.id.button_register:
+                    startActivity(new Intent(this, RegisterActivity.class));
+                    break;
+            }
+        }
+/*
+        button_register.setOnClickListener(()-> {
+            Intent register_intent = new Intent(MainActivity.this, RegisterActivity.class);
+            MainActivity.this.startActivity(register_intent);
+        });
+        */
+    }
+
