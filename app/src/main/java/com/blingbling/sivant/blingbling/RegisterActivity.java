df4cadaca36df87104059efbc1a588eaf156cdbc
sivant <sivantc1990@gmail.com>
2016-10-17 03:17:21 +0300

    3

--- app/src/main/java/com/blingbling/sivant/blingbling/RegisterActivity.java
+++ app/src/main/java/com/blingbling/sivant/blingbling/RegisterActivity.java
@@ -3,9 +3,12 @@ package com.blingbling.sivant.blingbling;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private static SeekBar seekBar_distance;
+    private static TextView km;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
