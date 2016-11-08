d294cdbb9a6b2119d22856d8195272452eec9f11
sivant <sivantc1990@gmail.com>
2016-11-08 16:28:49 +0200

    database login1

--- app/src/main/java/com/blingbling/sivant/blingbling/RegisterRequest.java
+++ app/src/main/java/com/blingbling/sivant/blingbling/RegisterRequest.java
@@ -16,16 +16,16 @@ public class RegisterRequest extends StringRequest {
     private static final String REGISTER_REQUEST_URL = "http://blingbling.netne.net/Register.php";
     private Map<String,String> params;
 
-    public RegisterRequest(String first_name, String last_name,String user_name,String password,String email,int radios,boolean popup_coupons,Response.Listener<String> listener) {
+    public RegisterRequest(String first_name, String last_name,String username,String password,String email,int radios, Response.Listener<String> listener) {
         super(Method.POST, REGISTER_REQUEST_URL, listener, null);
         params= new HashMap<>();
         params.put("first_name",first_name);
         params.put("last_name",last_name);
-        params.put("user_name",user_name);
+        params.put("username",username);
         params.put("password",password);
         params.put("email",email);
         params.put("radios", radios+"");
-        params.put("popup_coupons", popup_coupons+"");
+  //      params.put("popup_coupons", popup_coupons+"");
     }
 
     // @Override
